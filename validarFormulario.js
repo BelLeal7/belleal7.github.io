bc7a731aa29b6f79a1f9812f30c0e8b6200a91e1
BelLeal7 <bel7leal@gmail.com>
2024-05-25 03:52:46 -0300

    Un poco de todo, inicio del formulario
    Inicio del formulario
    Validacion en js
    Acomode la barra de inicio
    Agregue una imagen para que sea de referencia ed la paleta de colores(No incluida en el codigo)

---
+++ validarFormulario.js
@@ -0,0 +1,11 @@
+    function validar(){
+        const nombre=document.getElementsById("nombre");
+        const email=document.getElementById("email");
+        const tipoConsulta=document.getElementById("tipoConsulta");
+        const consulta=document.getElementById("consulta");
+        const tamano=document.getElementById("tamano");
+        const texto=document.getElementById("texto");
+
+        console.log(nombre,email,tipoConsulta,consulta,tamano,texto);
+
+    }
